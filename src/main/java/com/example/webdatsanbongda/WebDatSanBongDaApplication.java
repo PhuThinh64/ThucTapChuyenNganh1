b76d1d06b2c65fcf4666e6ba82c2c2bb83c8d029
thinh <thinh@gmail.com>
2025-10-30 17:17:39 +0700

    Hoan thanh layout

---
+++ src/main/java/com/example/webdatsanbongda/WebDatSanBongDaApplication.java
@@ -0,0 +1,13 @@
+package com.example.webdatsanbongda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebDatSanBongDaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebDatSanBongDaApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/webdatsanbongda/controller/HomeController.java
@@ -0,0 +1,41 @@
+package com.example.webdatsanbongda.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+    @GetMapping("/booking")
+    public String booking(){
+        return "booking";
+    }
+    @GetMapping("/checkout")
+    public String checkout(){
+        return "checkout";
+    }
+    @GetMapping("/contact")
+    public String contact(){
+        return "contact";
+    }
+    @GetMapping("/venue-detail")
+    public String venueDetail(){
+        return "venue-detail";
+    }
+    @GetMapping("/venues")
+    public String venues(){
+        return "venues";
+    }
+    @GetMapping("/account")
+    public String account(){
+        return "account";
+    }
+    @GetMapping("/about")
+    public String about(){
+        return "about";
+    }
+
+}
